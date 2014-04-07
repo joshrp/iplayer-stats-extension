ec23cf5d9faad06f03e5864dc5ff1f8bc9656a50
Josh Priestley <josh.priestley@bbc.co.uk>
2014-04-07 17:21:52 +0100

    Mark rows and columns of groups

--- hit-track.js
+++ hit-track.js
@@ -8,24 +8,42 @@ function getCounter() {
 
 run = function ($) {
 	console.log($('.stream-item'))
+
+	var column = 1, row = 'A'
 	$('.stream-item').each(function (i, item) {
 		$item = $(item)
 
 		if ($item.is('.group-item')) {
+			var itemNum = 1;
 			$item.find('.collection-list-item a').each(function (j, link) {
 				counter = getCounter()
-				counter.find('.count').text(i + '-' + j)
+				counter.find('.count').text(column + '_AB_' + itemNum)
 
 				$(link)
 					.css({position:'relative'})
+					.addClass(column + '_AB_' + itemNum)
 					.append(counter)
 
+				itemNum++;
 			});
+
+			row = 'A'
+			column++;
+
 		} else {
-			counter = getCounter()
-			counter.find('.count').text(i)
+			counter = getCounter();
+			counter.find('.count').text(column + '_' + row)
 			$(item).css({position:'relative'});
-			$item.find('a').eq(0).append(counter)
+			$item.find('a').eq(0)
+				.append(counter)
+				.addClass(column + '_' + row)
+
+			if (row == 'A') {
+				row = 'B'
+			} else {
+				row = 'A';
+				column++;
+			}
 		}
 
 	})
