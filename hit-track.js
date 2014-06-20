a3442cd9f03bfb47d70fe8ca22d185c3c9f5295f
Josh Priestley <josh.priestley@bbc.co.uk>
2014-06-20 16:34:16 +0100

    Add fading to show changes to stats and fill all groups with 5 items

--- hit-track.js
+++ hit-track.js
@@ -1,4 +1,4 @@
-function getCounter() {
+function getNewCounter() {
 	return $("<div/>", {
 		"class": "hit-count",
 		html: $('<span class="count"/>')
@@ -17,6 +17,9 @@ getCount = function (column, row, hours, stats) {
 		end = parseInt(hours[1], 10),
 		totalCount = 0;
 
+	if (item == undefined)
+		return false;
+
 	for (var i=0; i < end - start; i++) {
 		index = i + start;
 		totalCount += parseInt(item[formatHour(index)], 10) || 0;
@@ -26,16 +29,37 @@ getCount = function (column, row, hours, stats) {
 
 run = function ($, stats, hours) {
 	var column = 1, row = 'A'
-	$('.hit-count').remove();
+	$('.hit-count').css({opacity:1});
 
 	$('.stream-item').each(function (i, item) {
 		$item = $(item)
+		var counter = $item.find('.hit-count')
+		if (counter.length == 0)
+			counter = getNewCounter()
+
 		if ($item.is('.group-item')) {
 			var itemNum = 1;
+			var numChildren = $item.find('.collection-list-item a').length;
+
+			// Make sure every group has 5 items
+			if (numChildren < 5) {
+				children = $item.find('.collection-list-item');
+				itemsNeeded = 5 - numChildren;
+				newItems = children.slice(0,itemsNeeded).clone()
+				newItems.find('.title, .subtitle').html('');
+				newItems.find('a').attr('title','')
+				$item.find('.collection-list').append(newItems);
+			}
 
 			$item.find('.collection-list-item a').each(function (j, link) {
-				counter = getCounter()
-				counter.find('.count').text(getCount(column, 'AB_' + itemNum, hours, stats))
+				var counter = $(link).find('.hit-count')
+				if (counter.length == 0)
+					counter = getNewCounter()
+
+				count = getCount(column, 'AB_' + itemNum, hours, stats);
+				if (count === false)
+					count = 'No Data'
+				counter.find('.count').text(count)
 
 				$(link)
 					.css({position:'relative'})
@@ -53,7 +77,6 @@ run = function ($, stats, hours) {
 				return;
 			}
 
-			counter = getCounter();
 			counter.find('.count').text(getCount(column, row, hours, stats))
 			$(item).css({position:'relative'});
 			$item.find('a').eq(0)
@@ -69,6 +92,10 @@ run = function ($, stats, hours) {
 		}
 
 	})
+
+	setTimeout(function () {
+		$('.hit-count').css({opacity: 1})
+	}, 1)
 
 	groups = $('.stream-item.group-item');
 
